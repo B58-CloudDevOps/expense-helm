1cfaa2a1e94186cead8de957d9d6b43fc4cf763e
CodingManoj <CodingManoj@gmail.com>
2024-11-27 19:49:11 -0500

    Making Slim Image - Fix without alpine

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+aws eks update-kubeconfig --name "${1}-eks"
 ARGO_URL=$(kubectl get svc -n argocd|grep argocd-server |grep LoadBalancer | awk  '{print $4}')
 # ARGO_URL="argocd.cloudapps.today"
 ARGO_PWD=$(kubectl get secret argocd-initial-admin-secret -n argocd-initial-admin-secret -o json -n argocd | jq '.data.password'| xargs | base64 -d)
